433c9a097cdfdec4c7fd446e8d58da496553cd89
Evan Buswell <ebuswell@gmail.com>
2013-12-07 16:29:45 -0800

    fixed archlinux build

--- config.def.mk
+++ config.def.mk
@@ -1,16 +1,18 @@
 PREFIX?=/usr/local
 INCLUDEDIR?=${PREFIX}/include
 LIBDIR?=${PREFIX}/lib
+DESTDIR?=
 PKGCONFIGDIR?=${LIBDIR}/pkgconfig
 ARCH?=$(shell uname -m)
 # ARCH?=x86_64
 # ARCH?=i686
 
 CC?=gcc
-CFLAGS?=-Wall -Wextra -Wmissing-prototypes -Wredundant-decls -Wdeclaration-after-statement \
-       	-Og -g3
+CFLAGS?=-Og -g3
 LDFLAGS?=
+AR?=ar
 ARFLAGS?=rv
 
+CFLAGS+=-Wall -Wextra -Wmissing-prototypes -Wredundant-decls -Wdeclaration-after-statement
 CFLAGS+=-fplan9-extensions
 CFLAGS+=-Iinclude -Iinclude/${ARCH}
